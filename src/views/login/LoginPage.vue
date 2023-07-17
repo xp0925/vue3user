83c04c0b45578bd1e5659a6fd41872071e1fcdf8
jepson <jiangpeng@itcast.cn>
2023-07-18 02:56:38 +0800

    自定义校验

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -9,9 +9,16 @@ const formModel = ref({
   repassword: ''
 })
 // 整个表单的校验规则
-// 1. 非空校验 required: true   message消息提示，trigger触发校验的时机 blur change
+// 1. 非空校验 required: true      message消息提示，  trigger触发校验的时机 blur change
 // 2. 长度校验 min:xx, max: xx
 // 3. 正则校验 pattern: 正则规则    \S 非空字符
+// 4. 自定义校验 => 自己写逻辑校验 (校验函数)
+//    validator: (rule, value, callback)
+//    (1) rule  当前校验规则相关的信息
+//    (2) value 所校验的表单元素目前的表单值
+//    (3) callback 无论成功还是失败，都需要 callback 回调
+//        - callback() 校验成功
+//        - callback(new Error(错误信息)) 校验失败
 const rules = {
   username: [
     { required: true, message: '请输入用户名', trigger: 'blur' },
@@ -22,6 +29,25 @@ const rules = {
     {
       pattern: /^\S{6,15}$/,
       message: '密码必须是 6-15位 的非空字符',
+      trigger: 'blur'
+    }
+  ],
+  repassword: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码必须是 6-15位 的非空字符',
+      trigger: 'blur'
+    },
+    {
+      validator: (rule, value, callback) => {
+        // 判断 value 和 当前 form 中收集的 password 是否一致
+        if (value !== formModel.value.password) {
+          callback(new Error('两次输入密码不一致'))
+        } else {
+          callback() // 就算校验成功，也需要callback
+        }
+      },
       trigger: 'blur'
     }
   ]
@@ -76,8 +102,9 @@ const rules = {
             placeholder="请输入密码"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="repassword">
           <el-input
+            v-model="formModel.repassword"
             :prefix-icon="Lock"
             type="password"
             placeholder="请输入再次密码"
