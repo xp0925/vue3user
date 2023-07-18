2a9256d18e257cc185f441e767cc062f6a192f79
jepson <jiangpeng@itcast.cn>
2023-07-18 11:03:47 +0800

    弹层表单准备

--- src/views/article/components/ChannelEdit.vue
+++ src/views/article/components/ChannelEdit.vue
@@ -1,12 +1,33 @@
 <script setup>
 import { ref } from 'vue'
 const dialogVisible = ref(false)
+const formModel = ref({
+  cate_name: '',
+  cate_alias: ''
+})
+const rules = {
+  cate_name: [
+    { required: true, message: '请输入分类名称', trigger: 'blur' },
+    {
+      pattern: /^\S{1,10}$/,
+      message: '分类名必须是 1-10 位的非空字符',
+      trigger: 'blur'
+    }
+  ],
+  cate_alias: [
+    { required: true, message: '请输入分类别名', trigger: 'blur' },
+    {
+      pattern: /^[a-zA-Z0-9]{1,15}$/,
+      message: '分类名必须是 1-15 位的字母或数字',
+      trigger: 'blur'
+    }
+  ]
+}
 
 // 组件对外暴露一个方法 open，基于open传来的参数，区分添加还是编辑
 // open({})  => 表单无需渲染，说明是添加
 // open({ id, cate_name, ... })  => 表单需要渲染，说明是编辑
 // open调用后，可以打开弹窗
-
 const open = (row) => {
   console.log(row)
   dialogVisible.value = true
@@ -20,7 +41,25 @@ defineExpose({
 
 <template>
   <el-dialog v-model="dialogVisible" title="添加弹层" width="30%">
-    <div>渲染表单</div>
+    <el-form
+      :model="formModel"
+      :rules="rules"
+      label-width="100px"
+      style="padding-right: 30px"
+    >
+      <el-form-item label="分类名称" prop="cate_name">
+        <el-input
+          v-model="formModel.cate_name"
+          placeholder="请输入分类名称"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="分类别名" prop="cate_alias">
+        <el-input
+          v-model="formModel.cate_alias"
+          placeholder="请输入分类别名"
+        ></el-input>
+      </el-form-item>
+    </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
